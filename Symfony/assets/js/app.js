7ee45e96d408733b928a3364513da7ba4b2f2b64
Ccile <cecilisse@gmail.com>
2019-07-26 17:22:11 +0200

    Css et Js avec Encore ok, pages Accueil et Home ok, MatIcon ok, prêt pour login, user et CV !

--- Symfony/assets/js/app.js
+++ Symfony/assets/js/app.js
@@ -8,17 +8,28 @@
 // any CSS you require will output into a single css file (app.css in this case)
 import '../css/app.css';
 import '../css/bootstrap.min.css';
+import '../css/material.min.css';
+import '../css/maticons.css';
+import '../css/home.css';
+
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 // loads the jquery package from node_modules
-import $ from 'jquery';
+//import $ from 'jquery';
+// create global $ and jQuery variables
+//global.$ = global.jQuery = $;
+
 
 // import the function from greet.js (the .js extension is optional)
 // ./ (or ../) means to look for a local file
-import script from './script';
+import './script';
 
+import truc from './script';
 $(document).ready(function() {
-    script('Djedjill');
-});
+    truc("ça éblouit.");
+ });
+
+
 
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
--- Symfony/assets/js/script.js
+++ Symfony/assets/js/script.js
@@ -1,3 +1,30 @@
-export default function(name) {
-    console.log( `Yo yo ${name} - welcome to Encore!`);
-};
+export default function(truc) {
+console.log("Oua, " + truc); 
+}
+
+jQuery(document).ready(function($) {
+    $(".dropdown-item img").css({"height": "15px", "width": "15px"});
+    $("nav li").css("padding-left", "10px");
+
+    $(".dropdown-item").on("click", function(){
+        let colorid = $(this).attr("id");
+        if (colorid == "000000"){
+            $(".navbar.navbar-expand-lg").removeClass().addClass("navbar navbar-expand-lg navbar-dark");  
+        } else {
+            $(".navbar.navbar-expand-lg").removeClass().addClass("navbar navbar-expand-lg navbar-light");}
+
+        $(".navbar").css("background-color", "#"+colorid);
+        // $("footer").css("background-color", "#"+colorid);
+        $("footer").css("backgroundColor", $("nav").css("backgroundColor"));
+
+    });
+        let couleur = $("nav").css("backgroundColor");
+        $("footer").css("backgroundColor", couleur);
+
+
+        $("footer img").css({"height": "40px", "width": "auto","margin": "0px 10px 10px"});
+        $("footer").css("padding", "10px 30px 10px 30px");
+        $("footer").css("color","#666666");
+        $("footer a").css("color","#666666");
+
+});
